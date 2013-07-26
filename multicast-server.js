fab6e0a39e2b7b41b80a4ecddff4cc6ed7db5c7c
Sandro Pasquali <spasquali@appnexus.com>
2013-07-26 11:33:05 -0400

    bump

--- multicast-server.js
+++ multicast-server.js
@@ -10,17 +10,17 @@ var news = [
 
 var dgram = require('dgram'); 
 var server = dgram.createSocket("udp4"); 
-server.bind(PORT);
+server.bind(PORT, "0.0.0.0");
 
 server.on("listening", function() {
 
 	server.setBroadcast(true)
 	server.setMulticastTTL(128);
-	server.addMembership('68.67.148.72'); 
+	server.addMembership('localhost'); 
 
 	function broadcastNew() {
 		var message = new Buffer(news[Math.floor(Math.random()*news.length)]);
-		server.send(message, 0, message.length, 8088, "68.67.148.72");
+		server.send(message, 0, message.length, PORT, "0.0.0.0");
 		console.log("Sent " + message + " to the wire...");
 		//server.close();
 	}
