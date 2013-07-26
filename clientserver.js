f96d865c4da1e697d09a230ed026fdddd58ce0e5
Sandro Pasquali <spasquali@appnexus.com>
2013-07-26 11:26:24 -0400

    first

---
+++ clientserver.js
@@ -0,0 +1,44 @@
+var dgram = require("dgram");
+
+//	Create a udp4 server
+//
+var server = dgram.createSocket("udp4");
+
+//	Messages are handled here
+//
+server.on("message", function(msg, info) {
+	console.log("server got: " + msg + " from " +
+	info.address + ":" + info.port);
+});
+
+//	Server listening on localhost, port 41234.
+//	Callback is fired once socket is listening.
+//
+server.bind(41234, function() {
+
+	var address = server.address();
+	console.log("server listening " + address.address + ":" + address.port);
+
+	//	Create a client;
+	//
+	var client = dgram.createSocket("udp4");
+	
+	var message = new Buffer("UDP says Hello!");
+
+	//	Send a message
+	//
+	client.send(message, 0, message.length, 41234, "localhost", function(err, bytes) {
+		client.close();
+	});
+});
+
+//	Optionally, you may want to handle the "listening" event, which 
+//	functions like the #bind callback, above:
+//
+//	server.bind(41234) <- no callback
+//
+//	server.on("listening", function() {
+//		var address = server.address();
+//		console.log("server listening " + address.address + ":" + address.port);
+//	});
+//
---
+++ multicast-server.js
@@ -0,0 +1,30 @@
+var PORT = 8088;
+var news = [
+   "Borussia Dortmund wins German championship",
+   "Tornado warning for the Bay Area",
+   "More rain for the weekend",
+   "Android tablets take over the world",
+   "iPad2 sold out",
+   "Nation's rappers down to last two samples"
+];
+
+var dgram = require('dgram'); 
+var server = dgram.createSocket("udp4"); 
+server.bind(PORT);
+
+server.on("listening", function() {
+
+	server.setBroadcast(true)
+	server.setMulticastTTL(128);
+	server.addMembership('68.67.148.72'); 
+
+	function broadcastNew() {
+		var message = new Buffer(news[Math.floor(Math.random()*news.length)]);
+		server.send(message, 0, message.length, 8088, "68.67.148.72");
+		console.log("Sent " + message + " to the wire...");
+		//server.close();
+	}
+	
+	setInterval(broadcastNew, 3000);
+}
+
